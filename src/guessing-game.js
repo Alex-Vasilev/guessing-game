c9266ac49fc2b786d83b62616dac3f713d5e1696
Alex-Vasilev <aleksandr_vasilev1989@list.ru>
2017-01-28 01:50:17 +0300

    Game Set Range

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
     constructor() {
 //        this.number;
+      this.min;
+      this.max;
+        
     }
 
     setRange(min, max) {
 //        var self = this;
-  var result;
-        return result = ((function randomInteger(min, max) {
-        var rand = min + Math.random() * (max - min);
-                rand = Math.round(rand);
-                return rand;
-        })(min, max));
+
 //        console.log(this.number)
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+        
+            return (((function randomInteger(min, max) {
+        var rand = min + Math.random() * (max - min);
+                rand = Math.round(rand);
+                return rand;
+        })(this.min, this.max)));
+//        console.log(this.number)
     }
 
     lower() {
@@ -31,4 +37,6 @@ module.exports = GuessingGame;
 
         const number = 409;
         const game = new GuessingGame();
-        game.setRange(0, 4048)
+        game.setRange(0, 4048);
+        game.guess()
+        
