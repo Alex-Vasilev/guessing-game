8f010bc8728fd9a464b2bcc7fdfa7615bd753bae
Alex-Vasilev <aleksandr_vasilev1989@list.ru>
2017-01-31 02:45:47 +0300

    final-version

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,40 +1,16 @@
 class GuessingGame {
     constructor() {
-        this.result = null;
+      this.result = null;
       this.min = null;
-      this.max = null;
-        
+      this.max = null;       
     }
 
     setRange(min, max) {
-//        var self = this;
-
-//        console.log(this.number)
       this.min = min;
       this.max = max;
       
       return this;
     }
-
-//    guess() {
-//        
-//       this.result = ((function randomInteger(min, max) {
-//        var rand = min + Math.random() * (max - min);
-//                rand = Math.round(rand);
-//                return rand;
-//        })(this.min, this.max));
-////        console.log(this.result)
-//        return this.result;
-////        console.log(this.number)
-//    }
-//
-//    lower() {
-//
-//    }
-//
-//    greater() {
-//
-//    }
 
     guess() {
         this.result = Math.round((this.min + this.max) / 2);
